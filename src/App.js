e9cb333d48f38178dab9bd3dc03b6ecd84a03ec9
cyrus sarwale <cyrus.sarwale@misterspex.de>
2020-12-25 00:12:28 +0100

    filter search field

--- src/App.js
+++ src/App.js
@@ -19,13 +19,18 @@ class App extends Component {
   };
 
   render() {
+    const {monsters, searchField} = this.state; // the same as const monsters = this.state.mosters and const searchField = this.state.searchField
+    const filteredMonsters = monsters
+      .filter(monster => monster.name.toLowerCase()
+      .includes(searchField.toLowerCase()))
+
     return (
       <div className="App">
         <input
           type="search" placeholder="search monsters"
           onChange={e => this.setState({ searchField: e.target.value })}
         />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
